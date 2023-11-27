77bf636374cb6b227a86225936d482335c6f1cc3
wang-jing-xu <2731920653@qq.com>
2023-11-27 14:53:54 +0800

    本地代码上传

---
+++ classification_ModelNet40/models/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+from .pointmlp import pointMLP, pointMLPElite
+from .curvenet import CurveNet
+from .gbnet import GBNet
+from .gdanet import GDANET
+from .paconv import PAConv
+from .pct import PCT
+from .pointconv import PointConv
+from .pointnet import PointNet
+from .pointnet2 import PointNet2
+from .pointcont import PointConT_cls
