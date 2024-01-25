f8b06bc3b7d5310b07fe9481ffe7aab32c43e1c5
Jingxu Wang <74650916+wang-jing-xu@users.noreply.github.com>
2024-01-25 16:18:44 +0800

    Add files via upload

--- classification_ModelNet40/models/__init__.py
+++ classification_ModelNet40/models/__init__.py
@@ -2,12 +2,20 @@ from __future__ import absolute_import
 
 from .pointmlp import pointMLP, pointMLPElite
 from .curvenet import CurveNet
+from .curvenet_A import CurveNet_A
+from .curvenet_B import CurveNet_B
+from .curvenet_C import CurveNet_C
+from .curvenet_D import CurveNet_D
+from .curvenet_E import CurveNet_E
+from .curvenet_F import CurveNet_F
 from .gbnet import GBNet
 from .gdanet import GDANET
-from .paconv import PAConv
+# from .paconv import PAConv
 from .pct import PCT
 from .pointconv import PointConv
 from .pointnet import PointNet
 from .pointnet2 import PointNet2
 from .pointcont import PointConT_cls
+# from .pointcont3 import PointConT_cls
+
 from .apes import APES
